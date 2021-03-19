539a60e8f3a998354ed2c03653017dc1aeacae5c
andreayalad <alejandrayala01@gmail.com>
2021-03-18 22:46:00 -0600

    First component

---
+++ App2/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from './Components/FriendContact.vue';
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: 1,
+          name: "Andrea Ayala",
+          phone: "7225-3923",
+          email: "andreaayala@gmail.com",
+        },
+        {
+          id: 2,
+          name: "Silvia Ayala",
+          phone: "7455-3923",
+          email: "silayala@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
