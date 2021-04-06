f63da0d2f34425a64e9dd549f5a1028624839011
andreayalad <alejandrayala01@gmail.com>
2021-04-06 14:42:11 -0600

    Emmits

--- App2/src/Components/FriendContact.vue
+++ App2/src/Components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <h2>{{ name }} {{ isFavorite ? '(Fav)' : '' }}</h2>
+    <h2>{{ name }} {{ isFavorite ? "(Fav)" : "" }}</h2>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? "Hide" : "Show" }} Details
     </button>
@@ -14,36 +14,47 @@
 
 <script>
 export default {
- // props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
- props:{
-   id:{
-     type: String,
-     required: true
-   },
-   name: {
-     type: String,
-     required: true
-   },
-   phoneNumber:  {
-     type: String,
-     required: true
-   },
-   emailAddress:  {
-     type: String,
-     required: true
-   },
-   isFavorite:  {
-     type: Boolean,
-     required: false,
-     default: false,
-    //  validator: function(value){
-    //    return value === '1' || value === '0';
-    //  }
-   },
- },
+  // props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      //  validator: function(value){
+      //    return value === '1' || value === '0';
+      //  }
+    },
+  },
+  emits: ["toggle-favorite"],
+  // emits:{
+  //   'toggle-favorite': function(id) {
+  //     if(id){
+  //       return true
+  //     }else{
+  //       console.warn('Id is missing')
+  //       return false
+  //     }
+  //   }
+  // },
   data() {
     return {
-      detailsAreVisible: false
+      detailsAreVisible: false,
     };
   },
   methods: {
@@ -51,8 +62,8 @@ export default {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
     toggleFavorites() {
-      this.$emit('toggle-favorite', this.id);
-      }
+      this.$emit("toggle-favorite", this.id);
     },
-  }
+  },
+};
 </script>
