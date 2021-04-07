d0dcdb2d074f9447eabef1e82aa5fb9b53efd334
andreayalad <alejandrayala01@gmail.com>
2021-04-06 21:06:42 -0600

    Adding NewFriend component and connecting it

--- App2/src/App.vue
+++ App2/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <section>
     <header><h1>My Friends</h1></header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -19,23 +20,25 @@
 
 <script>
 import FriendContact from "./Components/FriendContact.vue";
+import NewFriend from './Components/NewFriend.vue';
 export default {
-  components: { FriendContact },
+  components: { FriendContact, NewFriend},
   data() {
+
     return {
       friends: [
         {
-          id: 'one',
-          name: 'Andrea Ayala',
-          phone: '7225-3923',
-          email: 'andreaayala@gmail.com',
+          id: "one",
+          name: "Andrea Ayala",
+          phone: "7225-3923",
+          email: "andreaayala@gmail.com",
           isFavorite: true,
         },
         {
-          id: 'two',
-          name: 'Silvia Ayala',
-          phone: '7455-3923',
-          email: 'silayala@gmail.com',
+          id: "two",
+          name: "Silvia Ayala",
+          phone: "7455-3923",
+          email: "silayala@gmail.com",
           isFavorite: false,
         },
       ],
@@ -48,6 +51,16 @@ export default {
       );
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
     },
+    addContact(name, phone, email){
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name : name,
+        phone: phone, 
+        email: email, 
+        isFavorite: false
+      }
+      this.friends.push(newFriendContact)
+    }
   },
 };
 </script>
@@ -84,7 +97,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -116,5 +130,18 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div{
+  margin: 1rem 0;
 }
 </style>
